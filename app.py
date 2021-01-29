884794b2247c2d92c04995a73283f00577c21ef3
Sini Liu <aisinilandia@gmail.com>
2021-01-29 15:22:24 +0200

    toimiva sovellus

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask
+from flask import redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    sql = "SELECT id, topic, created_at FROM polls ORDER BY id DESC"
+    result = db.session.execute(sql)
+    polls = result.fetchall()
+    return render_template("index.html", polls=polls)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    topic = request.form["topic"]
+    sql = "INSERT INTO polls (topic, created_at) VALUES (:topic, NOW()) RETURNING id"
+    result = db.session.execute(sql, {"topic":topic})
+    poll_id = result.fetchone()[0]
+    choices = request.form.getlist("choice")
+    for choice in choices:
+        if choice != "":
+            sql = "INSERT INTO choices (poll_id, choice) VALUES (:poll_id, :choice)"
+            db.session.execute(sql, {"poll_id":poll_id, "choice":choice})
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/poll/<int:id>")
+def poll(id):
+    sql = "SELECT topic FROM polls WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic = result.fetchone()[0]
+    sql = "SELECT id, choice FROM choices WHERE poll_id=:id"
+    result = db.session.execute(sql, {"id":id})
+    choices = result.fetchall()
+    return render_template("poll.html", id=id, topic=topic, choices=choices)
+
+@app.route("/answer", methods=["POST"])
+def answer():
+    poll_id = request.form["id"]
+    if "answer" in request.form:
+        choice_id = request.form["answer"]
+        sql = "INSERT INTO answers (choice_id, sent_at) VALUES (:choice_id, NOW())"
+        db.session.execute(sql, {"choice_id":choice_id})
+        db.session.commit()
+    return redirect("/result/"+str(poll_id))
+
+@app.route("/result/<int:id>")
+def result(id):
+    sql = "SELECT topic FROM polls WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    topic = result.fetchone()[0]
+    sql = "SELECT c.choice, COUNT(a.id) FROM choices c LEFT JOIN answers a " \
+          "ON c.id=a.choice_id WHERE c.poll_id=:poll_id GROUP BY c.id"
+    result = db.session.execute(sql, {"poll_id":id})
+    choices = result.fetchall()
+    return render_template("result.html", topic=topic, choices=choices)
